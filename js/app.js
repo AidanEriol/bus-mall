bebff65123849119f4e5aa3614acfcce9fd376a1
Danny Danger <dannywithachance@gmail.com>
2018-02-27 19:23:22 -0800

    added image array. also still trying to help david troubleshoot

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function addListeners() {
     document.getElementById("carousel").addEventListener("click", registerPicClick)
 }
@@ -8,5 +10,22 @@ function registerPicClick(event) {
         console.log(event.target.src.substring(index + 1));
     }
 }
+
+var imagesArray = [
+    {name: "Bag", image: "images/bag.jpg" },
+    {name: "Banana", image: "images/banana.jpg"},
+    {name: "Boots", image: "images/boots.jpg"},
+    {name: "Chair", image: "images/chair.jpg"},
+    {name: "Cthulhu", image: "images/cthulhu.jpg"},
+    {name: "Dragon", image: "images/dragon.jpg"},
+    {name: "Pen", image: "images/pen.jpg"},
+    {name: "Scissors", image: "images/scissors.jpg"},
+    {name: "Shark", image: "images/shark.jpg"},
+    {name: "Sweep", image: "images/sweep.jpg"},
+    {name: "Unicorn", image: "images/unicorn.jpg"},
+    {name: "USB", image: "images/usb.jpg"},
+    {name: "Water Can", image: "images/water_can.jpg"},
+    {name: "Wine Glass", image: "images/wine_glass.jpg"}
+]
 
 window.addEventListener("load", addListeners);
