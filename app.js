9acd1e5931c45a14c0513046fdb24b0eec8b8f1f
Danny Danger <dannywithachance@gmail.com>
2018-02-27 19:04:03 -0800

    trying to help david with something

---
+++ app.js
@@ -0,0 +1,12 @@
+function addListeners() {
+    document.getElementById("carousel").addEventListener("click", registerPicClick)
+}
+
+function registerPicClick(event) {
+    if (event.target.tagName == "IMG") {
+        var index = event.target.src.lastIndexOf("/");
+        console.log(event.target.src.substring(index + 1));
+    }
+}
+
+window.addEventListener("load", addListeners);
