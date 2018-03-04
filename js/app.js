9dd4ab7c81d724747742651b67ac1308ab3bef8e
Davidchhing <davidhchhing@gmail.com>
2018-03-04 14:37:16 -0800

    made pictures change after user click

--- js/app.js
+++ js/app.js
@@ -7,24 +7,24 @@ function addListeners() {
 }
 
 function registerPicClick(event) {
-    if (event.target.tagName == "IMG") {         
-       var index = event.target.src.lastIndexOf("/");
-        var url = event.target.src.substring(index + 1);   
+    if (event.target.tagName == "IMG") {
+        var index = event.target.src.lastIndexOf("/");
+        var url = event.target.src.substring(index + 1);
         console.log(url);
         for (var imageIndex = 0; imageIndex < imagesArray.length; imageIndex++) {
             var item = imagesArray[imageIndex];
             if (item.url.indexOf(url) !== -1) {
-            item.clicks++;
+                item.clicks++;
                 console.log(item);
             }
-           
         }
-        
         counter++;
+        showRandomItem();
     }
+
 }
 
-var Item = function(name, url) {
+var Item = function (name, url) {
     this.name = name;
     this.url = url;
     this.clicks = 0;
@@ -54,17 +54,17 @@ function randomItemUrl() {
     return imagesArray[randomize].url;
 }
 
-var showRandomItem = function() {
+var showRandomItem = function () {
     var urlArray = [];
     var showItem = document.getElementById("carousel");
-
-    while (urlArray.length != 3) {
+    showItem.innerHTML = "";
+    while (urlArray.length != 3) { 
         var currentUrl = randomItemUrl();
         if (urlArray.indexOf(currentUrl) == -1) {
             urlArray.push(currentUrl);
         }
     };
-console.log(urlArray);
+    console.log(urlArray);
     for (var index = 0; index < urlArray.length; index++) {
         var img = document.createElement("img");
         img.setAttribute("src", urlArray[index]);
@@ -72,6 +72,6 @@ console.log(urlArray);
     }
 }
 
-console.log(showRandomItem());
+showRandomItem();
 
 window.addEventListener("load", addListeners);
